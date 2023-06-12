b735b3730fa61a8246bcf9b2354a5e568e4d80ee
Mako0722 <zhongtiancheng79@gmail.com>
2023-06-12 20:59:15 +0900

    first commit

---
+++ TableViewandAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewandAPI
+//
+//  Created by 中田誠 on 2023/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
